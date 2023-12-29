bbef421ba46f1e83a3fe3670e9fae72b47484675
Henrique Martins Botelho <henriquemartinsbotelho@gmail.com>
2023-12-29 13:30:35 -0300

    first

---
+++ web_server/index.js
@@ -0,0 +1,8 @@
+import * as net from "net";
+function newConn(socket) {
+    console.log("new connection", socket.remoteAddress, socket.remotePort);
+    // ...
+}
+let server = net.createServer();
+server.on("connection", newConn);
+server.listen({ host: "127.0.0.1", port: 1234 });
